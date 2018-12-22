f2c2294f5d21fcbfff5f323446c282bde6c174cf
Christopher Hill <chill.signup@gmail.com>
2018-12-22 20:08:57 +0000

    v3

--- cookbooks/apache2/recipes/default.rb
+++ cookbooks/apache2/recipes/default.rb
@@ -14,6 +14,18 @@ service 'apache2' do
   supports status: true
   action [:enable, :start]
 end
+
+group 'web_admin'
+
+user 'web_admin' do
+  group 'web_admin'
+  system true
+  shell '/bin/bash'
+end
+
 template '/var/www/html/index.html' do
   source 'index.html.erb'
+  mode '0644'
+  owner 'web_admin'
+  group 'web_admin'
 end
