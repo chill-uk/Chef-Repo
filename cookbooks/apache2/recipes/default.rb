6a7a0871215c152e0810446038c3f2311b47bf32
Christopher Hill <chill.signup@gmail.com>
2018-12-22 19:36:35 +0000

    update

--- cookbooks/apache2/recipes/default.rb
+++ cookbooks/apache2/recipes/default.rb
@@ -1,24 +1,19 @@
-#
 # Cookbook:: apache2
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
 
 apt_update 'Update the apt cache daily' do
-    frequency 86_400
-    action :periodic
+  frequency 86_400
+  action :periodic
 end
-  
 package 'apache2' do
-    action :install
+  action :install
 end
-    
-  
 service 'apache2' do
-    supports status: true
-    action [:enable, :start]
+  supports status: true
+  action [:enable, :start]
 end
-  
-template '/var/www/html/index.html' do # ~FC033
-    source 'index.html.erb'
+template '/var/www/html/index.html' do
+  source 'index.html.erb'
 end
